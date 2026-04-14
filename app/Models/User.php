3c02c4239480b3d2b21934c68d203bf2a84863e8
AlexLC642 <alexestuardo642@gmail.com>
2026-04-14 15:14:10 -0600

    Fix 403 error by implementing FilamentUser (v14)

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,8 +8,10 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasFactory, Notifiable, HasRoles, Auditable, SoftDeletes;
 
@@ -40,5 +42,10 @@ class User extends Authenticatable
     public function setEmailAttribute(?string $value): void
     {
         $this->attributes['email'] = $value ? strtolower($value) : null;
+    }
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return $this->is_active;
     }
 }
