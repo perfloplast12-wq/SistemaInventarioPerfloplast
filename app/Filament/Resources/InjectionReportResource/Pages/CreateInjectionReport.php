40156cf7340348e4297a8856661277ac4e94f411
AlexLC642 <alexestuardo642@gmail.com>
2026-04-19 22:58:37 -0600

    fix: syntax error in CreateInjectionReport

--- app/Filament/Resources/InjectionReportResource/Pages/CreateInjectionReport.php
+++ app/Filament/Resources/InjectionReportResource/Pages/CreateInjectionReport.php
@@ -3,16 +3,9 @@
 namespace App\Filament\Resources\InjectionReportResource\Pages;
 
 use App\Filament\Resources\InjectionReportResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateInjectionReport extends CreateRecord
 {
     protected static string $resource = InjectionReportResource::class;
-
-    protected function getRedirectUrl(): string
-    {
-        return $this->getResource()::getUrl('index');
-    }
-}
 }
