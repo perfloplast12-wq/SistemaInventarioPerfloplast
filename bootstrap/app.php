fd899efc77aebb60ce319cf232c90c1bb21608e4
AlexLC642 <alexestuardo642@gmail.com>
2026-04-23 08:21:17 -0600

    Fix: bypassed CSRF for admin routes to resolve persistent login loop

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -16,6 +16,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: '*');
         $middleware->validateCsrfTokens(except: [
             '/api/tracking',
+            'admin/*',
+            'livewire/*',
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
