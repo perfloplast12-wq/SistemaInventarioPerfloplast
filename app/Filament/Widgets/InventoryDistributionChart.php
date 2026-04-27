0591087224cd19ee8d6fd1bc370fd9c46ebbe749
AlexLC642 <alexestuardo642@gmail.com>
2026-04-27 12:08:57 -0600

    Emergency: Simplify Inventario to isolate 500 error

--- app/Filament/Widgets/InventoryDistributionChart.php
+++ app/Filament/Widgets/InventoryDistributionChart.php
@@ -15,13 +15,13 @@ class InventoryDistributionChart extends Widget
         $data = Product::query()
             ->where('type', 'raw_material')
             ->isActive()
-            ->withSum('stocks', 'quantity')
+            ->withSum('stocks as stocks_sum_quantity', 'quantity')
             ->get()
             ->filter(fn($p) => (float)($p->stocks_sum_quantity ?? 0) > 0)
             ->sortByDesc('stocks_sum_quantity');
 
         $labels = $data->pluck('name')->toArray();
-        $series = $data->pluck('stocks_sum_quantity')->map(fn($v) => (float)$v)->toArray();
+        $series = $data->pluck('stocks_sum_quantity')->map(fn($v) => (float)($v ?? 0))->toArray();
 
         return compact('labels', 'series');
     }
