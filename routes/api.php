aa906c0f05c3b88a08f1d225eb2766e7f2771a04
AlexLC642 <alexestuardo642@gmail.com>
2026-04-28 10:29:14 -0600

    Fix final: se importa TrackingController en api y se fuerza z-index del boton X

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\CatalogController;
+use App\Http\Controllers\Api\TrackingController;
 
 Route::get('/catalog', [CatalogController::class, 'index'])
     ->name('api.catalog.index');
