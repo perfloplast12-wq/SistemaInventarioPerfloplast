cccc02d11ec3bc45e7c0284bcfd9b465a4002b7e
AlexLC642 <alexestuardo642@gmail.com>
2026-04-30 14:11:17 -0600

    Fix: Renderizado manual del botón Volver en Apariencia

--- resources/views/filament/pages/appearance-settings.blade.php
+++ resources/views/filament/pages/appearance-settings.blade.php
@@ -2,7 +2,16 @@
     <form wire:submit="save">
         {{ $this->form }}
 
-        <div class="mt-6">
+        <div class="mt-6 flex items-center gap-4">
+            <x-filament::button 
+                tag="a" 
+                href="{{ \App\Filament\Resources\UserResource::getUrl('index') }}" 
+                color="gray" 
+                outlined
+            >
+                Volver
+            </x-filament::button>
+
             <x-filament::button type="submit" size="lg">
                 Guardar Cambios
             </x-filament::button>
