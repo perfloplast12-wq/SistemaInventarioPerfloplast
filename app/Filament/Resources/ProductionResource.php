c0e00ca31e766ea66bf1952485887acc87ab50dd
AlexLC642 <alexestuardo642@gmail.com>
2026-05-01 14:07:30 -0600

    Fix: Add missing Get and Set imports in ProductionResource

--- app/Filament/Resources/ProductionResource.php
+++ app/Filament/Resources/ProductionResource.php
@@ -6,6 +6,8 @@ use App\Filament\Resources\ProductionResource\Pages;
 use App\Models\Production;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
